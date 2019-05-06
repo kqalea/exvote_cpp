d729d4f0d26b6c0a87c7be5064b77ed740192cf2
Andy Chen <kqalea@gmail.com>
2019-05-06 14:40:15 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 6, 2019
+ *      Author: kqalea
+ */
+
+#include<iostream>
+
+int main(int argc, char* argv[]){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
